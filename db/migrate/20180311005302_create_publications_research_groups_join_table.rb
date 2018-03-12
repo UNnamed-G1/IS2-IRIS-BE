35ece5aa4f50aba0ef7064d28a0664c6e3976bca
Julián Salomón <jesalomont@unal.edu.co>
2018-03-12 15:54:37 -0500

    Correction to Publication and Research Group association

--- db/migrate/20180311005302_create_publications_research_groups_join_table.rb
+++ db/migrate/20180311005302_create_publications_research_groups_join_table.rb
@@ -1,8 +1,8 @@
 class CreatePublicationsResearchGroupsJoinTable < ActiveRecord::Migration[5.1]
   def change
     create_join_table :research_groups, :publications do |t|
-      t.references :research_group_id, foreign_key: true
-      t.references :publication_id, foreign_key: true
+      t.references :research_group, foreign_key: true
+      t.references :publication, foreign_key: true
     end
   end
 end
