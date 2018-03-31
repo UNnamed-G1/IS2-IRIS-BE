6823ce87e1f47765f9ef0ec038a60f4332402810
Julián Salomón <jesalomont@unal.edu.co>
2018-03-31 18:46:12 -0500

    + Added http production site on cors * Rename to type_u field in user

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', '/\Ahttps:\/\/is2-iris-fe.herokuapp.com\z/'
+    origins 'localhost:4200', '/\Ahttps:\/\/is2-iris-fe.herokuapp.com\z/', '/\Ahttp:\/\/is2-iris-fe.herokuapp.com\z/'
 
     resource '*',
       headers: :any,
