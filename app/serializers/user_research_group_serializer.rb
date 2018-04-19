6f2663e62c56b8b35d2f767d96bae588e80bd120
Julián Salomón <jesalomont@unal.edu.co>
2018-04-19 04:14:23 -0500

    - UserResearchGroup's inexistent attribute removed from Serializer

--- app/serializers/user_research_group_serializer.rb
+++ app/serializers/user_research_group_serializer.rb
@@ -25,13 +25,12 @@
 class UserResearchGroupSerializer < ActiveModel::Serializer
   type :member
   attributes :id, :joining_date, :end_joining_date, :state
-  attributes :hours_per_week
 
   attribute :type_urg, key: :member_type
 
   belongs_to :user
   belongs_to :research_group
-  
+
   def type_urg
     return object.type_urg.capitalize
   end
