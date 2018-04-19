1a5fb18254916b8dbc2cc81fc8580b82065a16fa
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2018-04-19 02:42:35 -0500

    + mailer welcome user to the research group

---
+++ app/mailers/research_group_mailer.rb
@@ -0,0 +1,16 @@
+class ResearchGroupMailer < ApplicationMailer
+
+    def welcome_research_group(recipient, research_group)
+        @user = recipient
+        @research_group = research_group
+
+        attachments.inline['iris_logo.png'] = File.read("#{Rails.root}#{path_images}/IRIS_logo.png")
+        attachments.inline['unal_logo.png'] = File.read("#{Rails.root}#{path_images}/UNAL_escudo.png")
+        attachments.inline['research_group_picture.png'] = File.read("#{Rails.root}/public#{@research_group.photo.picture}")
+
+        full_name = @user.name + " " + @user.lastname
+        email_with_name = %("#{full_name}" <#{@user.email}>)
+        mail(to: email_with_name, subject: "Ahora eres miembro de #{@research_group.name} !")
+    end
+
+end
---
+++ test/mailers/previews/research_group_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/research_group_mailer
+class ResearchGroupMailerPreview < ActionMailer::Preview
+
+end
