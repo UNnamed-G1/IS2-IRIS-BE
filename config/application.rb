90223da909ba000bb484bb7ae15b11b2821f1588
Jhon Sedano <jasedanod@unal.edu.co>
2018-04-21 17:48:11 -0500

    Added styles to reports

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require 'wicked_pdf'
-#require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
